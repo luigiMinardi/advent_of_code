06719a284a58484be677376b525bab53874d4283
Luigi <lugminsinlag@gmail.com>
2023-12-03 16:34:33 +0100

    feat(2023 - Go): AOC day2 part1 done.
    refactor: creating folders for each day instead of having everything in one folder.

---
+++ 2023/golang/day2/day2.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+    file, err := os.Open("input.txt")
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    file_scanner := bufio.NewScanner(file)
+    
+    var current_game_id = 1
+    var result = 0
+    for file_scanner.Scan() {
+        fmt.Println(file_scanner.Text())
+        var rgbmap map[string]int = make(map[string]int)
+
+        all_rounds := strings.Split(strings.Split(file_scanner.Text(), ": ")[1], "; ") // ["1 green, 3 red, 6 blue", "3 green, 6 red", "9 blue"]
+        for i := range all_rounds {
+            round_cubes := strings.Split(all_rounds[i], ", ") // ["1 green", "3 red", "6 blue"]
+            for j := range round_cubes {
+                cube_values := strings.Split(round_cubes[j], " ") // ["123", "red/green/blue"] 
+                v, _ := strconv.Atoi(cube_values[0])
+
+                if rgbmap[cube_values[1]] < v {
+                    rgbmap[cube_values[1]] = v
+                }
+                fmt.Printf("rounds: %v\n", rgbmap)
+            }
+        }
+        if rgbmap["red"] <= 12 && rgbmap["green"] <= 13 && rgbmap["blue"] <= 14 {
+            result += current_game_id
+        }
+        current_game_id++
+    }
+
+    fmt.Printf("result: %v\n", result)
+}
