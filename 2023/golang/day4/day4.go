e11d1f2b1878aaf898d0f2927632c636a1e75075
Luigi <lugminsinlag@gmail.com>
2023-12-06 22:01:05 +0100

    feat(2023 - Go): AOC day4 part2 done.

--- 2023/golang/day4/day4.go
+++ 2023/golang/day4/day4.go
@@ -14,20 +14,29 @@ func main() {
 
     var part1_answr int
 
+    var cards map[int][]int = make(map[int][]int)
+    var total_cards int = 0
+    var game int = 1
+
     for file_scanner.Scan() {
         var hasValid bool = false
         var roundPoints int = 1
+        var matches int = 0
 
         value := strings.Split(strings.TrimSpace(strings.Split(file_scanner.Text(), ": ")[1]), " | ")
 
         win_nums := strings.Split(strings.ReplaceAll(value[0], "  ", " "), " ")
         my_nums := strings.Split(strings.ReplaceAll(value[1], "  ", " "), " ")
-        fmt.Println(win_nums)
-        fmt.Println(my_nums)
 
         for _, num := range win_nums {
             for _, mnum := range my_nums {
                 if (num == mnum) {
+                    matches+= 1
+                    for range cards[game] {
+                        cards[game+matches] = append(cards[game+matches], game+matches)
+                    }
+                    if len(cards[game]) > 0 {
+                    }
                     if(!hasValid) {
                         hasValid = true
                     } else {
@@ -35,14 +44,29 @@ func main() {
                     }
                 }
             }
-            fmt.Println(num, hasValid, roundPoints)
         }
 
         if hasValid {
             part1_answr += roundPoints
-            fmt.Println(roundPoints, part1_answr)
+        } else {
+            cards[game] = append(cards[game], game)
         }
 
-        fmt.Println(part1_answr)
+        for match := 1; match <= matches; match++ {
+            if hasValid {
+                cards[game] = append(cards[game], game)
+                hasValid = false
+            }
+            cards[game+match] = append(cards[game+match], game+match)
+        }
+
+        game++
     }
+
+    for _, val := range cards {
+        total_cards += len(val)
+    }
+
+    fmt.Println("p1",part1_answr)
+    fmt.Println("p2",total_cards)
 }
