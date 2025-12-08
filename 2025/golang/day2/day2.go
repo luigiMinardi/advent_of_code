2b3396b87be8cded8a86de9deaf974ba8f14cbb7
Luigi Minardi <dev@su.luigiminardi.com>
2025-12-09 00:05:28 +0100

    feat(2025 - Go): AOC day2 part2 done.

--- 2025/golang/day2/day2.go
+++ 2025/golang/day2/day2.go
@@ -29,7 +29,7 @@ func onComma(data []byte, atEOF bool) (advance int, token []byte, err error) {
 	return 0, data, bufio.ErrFinalToken
 }
 
-func main() {
+func part1() {
 	// The ranges are separated by commas (,);
 	// each range gives its first ID and last ID separated by a dash (-).
 	// invalid IDs are any ID which is made only of some sequence of digits
@@ -37,7 +37,6 @@ func main() {
 	// None of the numbers have leading zeroes; 0101 isn't an ID at all.
 	// find all of the invalid IDs
 	// What do you get if you add up all of the invalid IDs?
-
 	file, err := os.Open("input.txt")
 	if err != nil {
 		fmt.Println(err)
@@ -88,4 +87,105 @@ func main() {
 		}
 	}
 	fmt.Println(sumInvalidIDs)
+}
+
+// Splits string str every N characters
+// Returns slice of substrings and if all substrings are equal
+func splitStrInNCharsReturnItAndIfEqual(str string, N int) ([]string, bool) {
+	var slice []string
+	for charIdx := 0; charIdx < len(str); charIdx += N {
+		if charIdx+N > len(str) {
+			slice = append(slice, str[charIdx:])
+			break
+		}
+		slice = append(slice, str[charIdx:charIdx+N])
+	}
+	isEqual := true
+	for v := 1; v < len(slice); v++ {
+		if slice[v] != slice[v-1] {
+			isEqual = false
+			break
+		}
+	}
+	return slice, isEqual
+}
+func part2() {
+	// The ranges are separated by commas (,);
+	// each range gives its first ID and last ID separated by a dash (-).
+	// ID is invalid if it is made only of some sequence of digits repeated
+	// at least twice. (12341234, 123123123, 1212121212, 1111111)
+	// None of the numbers have leading zeroes; 0101 isn't an ID at all.
+	// find all of the invalid IDs
+	// What do you get if you add up all of the invalid IDs?
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(onComma)
+
+	var sumInvalidIDs int
+
+	for scanner.Scan() {
+		text := scanner.Text()
+		var firstID int
+		var lastID int
+		for i, v := range text {
+			if v == '-' {
+				firstID, err = strconv.Atoi(text[:i])
+				if err != nil {
+					fmt.Println(err)
+				}
+				n := text[i+1:]
+				if n[len(n)-1] == '\n' {
+					// removing \n from slice
+					n = n[:len(n)-1]
+				}
+				lastID, err = strconv.Atoi(n)
+				if err != nil {
+					fmt.Println(err)
+				}
+				break
+			}
+		}
+
+		for i := firstID; i <= lastID; i++ {
+			iStr := strconv.Itoa(i)
+			half := len(iStr) / 2
+			// start checking half, if fails reduce testingLen until 1
+			for j := range half {
+				testingLen := half - j
+				left := iStr[:testingLen]
+				right := iStr[half+j:]
+				rightShift := iStr[half+j+1:]
+				// fixing odd flooring by ignoring middle index
+				if len(left) != len(right) {
+					// odd
+					if left == rightShift {
+						//fmt.Println("GOTTEM", i)
+						_, isEqual := splitStrInNCharsReturnItAndIfEqual(iStr, testingLen)
+						if isEqual {
+							sumInvalidIDs += i
+							break
+						}
+					}
+					continue
+				}
+				// even
+				if left == right {
+					//fmt.Println("GOTTEM", i)
+					_, isEqual := splitStrInNCharsReturnItAndIfEqual(iStr, testingLen)
+					if isEqual {
+						sumInvalidIDs += i
+						break
+					}
+				}
+			}
+		}
+	}
+	fmt.Println(sumInvalidIDs)
+}
+func main() {
+	part2()
 }
